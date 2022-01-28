3d985a6ce4ae8cba7b82335586a83811e821c03b
Wojciech Kosak-Główczewski <wojciech.kosak-glowczewski@clickmeeting.com>
2022-01-28 18:30:26 +0100

    unsigned int

--- clock_controller/clock_controller.ino
+++ clock_controller/clock_controller.ino
@@ -173,7 +173,7 @@ void send_positions() {
         byte *p = (byte *)dirs + i;
         Wire.write(*p);
       }    
-      unsigned int16_t q = set_speeds[xpos][ypos];
+      uint16_t q = set_speeds[xpos][ypos];
       for (i = 0; i < sizeof(q); i++) {
         byte *p = (byte *)&q + i;
         Wire.write(*p);
